5749b7f94533bb4d9246535598afc1f62ebb4fd9
gamer <openaixander@gmail.com>
2026-02-22 10:05:24 +0100

    chore: final production configurations and async email thread

--- accounts/models.py
+++ accounts/models.py
@@ -42,7 +42,7 @@ class CustomUserManager(BaseUserManager):
         if extra_fields.get('is_superuser') is not True:
             raise ValueError(_('Superuser must have is_superuser=True.'))
         
-        return self.createuser(email, password, **extra_fields)
+        return self.create_user(email, password, **extra_fields)
 
 
 class CustomUser(AbstractUser):
